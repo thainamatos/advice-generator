b2f505147786e90b6ca127fb08cad4380dae047c
Thainá Matos <thainamatos.if@hotmail.com>
2023-02-16 16:54:38 -0300

    final app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import "./App.css";
-import React, {useState, useEffect} from "react";
-import pauseDesktop from "./images/pattern-divider-desktop.svg";
-import pauseMobile from "./images/pattern-divider-mobile.svg";
-import icon from "./images/icon-dice.svg";
-import { useState } from "react/cjs/react.production.min";
+import React, { useState, useEffect } from "react";
+import dividerDesktop from './images/pattern-divider-desktop.svg'
+import dividerMobile from './images/pattern-divider-mobile.svg'
+import dice from './images/icon-dice.svg'
 import axios from "axios";
 
 function App() {
-  const [advice, setAdvice] = useState('');
+  const [advice, setAdvice] = useState("");
 
-  const getAdvice = async() =>{
+  const getAdvice = async () => {
     const response = await axios.get("https://api.adviceslip.com/advice");
-    console.log(response);
-  }
+    const advice = await response.data.slip;
+    setAdvice(advice);
+  };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
 
   return (
-    <div>
-      <main className="container">
-        <h1 className="title">Advice #</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem.
-        </p>
-        <picture>
-          <source media="(min-width: 768px)" srcSet={pauseDesktop} />
-          <img src={pauseMobile} alt="" />
-        </picture>
-        <button>
-          <img src={icon} alt="" />
-        </button>
-      </main>
+    <div className='card'>
+        <h1>ADVICE #{advice.id}</h1>
+        <p className="fade">“{advice.advice}”</p>
+        <img src={dividerDesktop} className="divider-desktop" alt="divider"  />
+        <img src={dividerMobile} className="divider-mobile" alt="divider" />
+        <div className="dice" onClick={getAdvice}>
+        <img src={dice} alt="dice"   />
+        </div>
     </div>
   );
 }
